ec567a972d2880ad71ac6f74555fedcad0bd2fdf
Florian Winkler <flo@wnklr.art>
2024-03-03 19:35:39 +0100

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,8 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Currently Connected, Too',
-	description: 'You Are Currently Connected To ...',
+	description:
+		'Creative collective in the making. Shaping art and culture. Coming soon.',
 };
 
 export default function RootLayout({
@@ -16,6 +17,25 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
+			<head>
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/apple-touch-icon.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/favicon-16x16.png'
+				/>
+			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
