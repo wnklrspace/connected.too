10369b5606de18dcc02195ab6b07ec6829aad226
Florian Winkler <flo@wnklr.art>
2024-03-03 16:35:23 +0100

    add content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import Image from 'next/image';
+import font from './typography.module.css';
 import styles from './page.module.css';
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<div className={styles['video-view']}></div>
+			<div className={styles['video-view']}>
+				<div className={styles.container}>
+					<Image
+						src='./connected.too_logo.svg'
+						alt=''
+						width={170}
+						height={27.12}
+					/>
+					<p className={`${font.paragraph} ${font.center}`}>
+						creative collective in the making. <br /> shaping art and culture.
+						<br /> coming soon.
+					</p>
+				</div>
+			</div>
 		</main>
 	);
 }
